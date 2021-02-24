a6142f26773d03fb9f54302bb269e5b24a6144b9
Ginger-Mano <mcasimirian@gmail.com>
2021-02-24 15:35:45 -0500

    first fetch incomplete

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 let appBody = document.querySelector('.main')
 
 
+let navBar = () => {
+    let bar = document.createElement('div')
+    bar.className = "navbar"
+
+    let navBarUl = document.createElement('div')
+    navBarUl.className = "navInner"
+
+    let navLiDiv1 = document.createElement('div')
+    navLiDiv1.className = "navlink"
+    navLiDiv1.innerText = "picYarn"
+
+    let navLiDiv2 = document.createElement('div')
+    navLiDiv2.className = "navlink"
+    navLiDiv2.innerText = "About"
+
+    let navLiDiv3 = document.createElement('div')
+    navLiDiv3.className = "navlink"
+    navLiDiv3.innerText = "Name"
+
+    // navLink1.append(navLiDiv1)
+    // navLink2.append(navLiDiv2)
+    // navLink3.append(navLiDiv3)
+
+    navBarUl.append(navLiDiv1, navLiDiv2, navLiDiv3)
+    bar.append(navBarUl)
+    appBody.append(bar)
+}
+navBar()
+
+
 let welcomeDiv = () => {
+
     let welcome = document.createElement('div')
     welcome.className = "welcome"
-    welcome.innerText = `<h1>"Welcome to picYarn!"</h1>`
+    welcome.innerText = "Welcome to picYarn!"
 
     appBody.append(welcome)
 }
+welcomeDiv()
+
+let photoCollect = () => {
+
+    fetch(`http://localhost:3000/photos`)
+        .then(res => res.json())
+        .then(photos =>
+            console.log(photos)
+        )
+
+
+}
+photoCollect()
+
+let photoCollage = (photos) => {
+    let photoDisplay = document.createElement('div')
+    photoDisplay.className = "photos"
+
+    let eachPhoto = document.createElement('img')
+    eachPhoto.src = photos.image
+}
+
