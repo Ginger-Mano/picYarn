682e96e9c9c5bd2c40a449f060719734bf3f111a
Manon Sainton <manonsainton.dev@gmail.com>
2021-04-09 11:29:45 -0400

    add story method

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,4 +3,16 @@ class StoriesController < ApplicationController
         story = Story.all
         render json: story
     end
+
+    def create
+        story = Story.create(story_params)
+        render json: story
+    end
+
+    private
+
+    def story_params
+        params.permit(:title, :likes, :wordcount, :text, :author, :user_id, :photo_id)
+    end
+
 end
